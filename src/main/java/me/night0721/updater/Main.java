3d6d04a1d75dcc2517bd8954a0ad71c4b0a4f1ed
NK <oscarfu0721@gmail.com>
2023-03-07 15:35:08 +0000

    shorter code, smaller file size

--- src/main/java/me/night0721/updater/Main.java
+++ src/main/java/me/night0721/updater/Main.java
@@ -2,10 +2,10 @@ package me.night0721.updater;
 
 import javax.swing.*;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 public class Main {
@@ -14,23 +14,13 @@ public class Main {
         File modFile = new File(args[1]);
         if (!modFile.delete()) {
             JOptionPane.showMessageDialog(null, "Update failed! Couldn't delete the file provided.");
-            return;
-        }
-        if (args[2].equals("mainrepo")) {
-            downloadFile(modFile.getPath());
-            JOptionPane.showMessageDialog(null, "Updated Aurora!");
             System.exit(0);
         }
-    }
-    private static void downloadFile(String filePath) throws IOException {
-        URL url = new URL("https://github.com/night0721/Lilase/releases/latest/download/Lilase.jar");
-        try (InputStream in = url.openStream();
-             FileOutputStream out = new FileOutputStream(filePath)) {
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) != -1) {
-                out.write(buffer, 0, bytesRead);
-            }
+        if (args[2].equals("mainrepo")) {
+            URL url = new URL("https://github.com/night0721/Lilase/releases/latest/download/Lilase.jar");
+            Files.copy(url.openStream(), Paths.get(modFile.getPath()));
+            JOptionPane.showMessageDialog(null, "Updated Aurora!");
+            System.exit(0);
         }
     }
 }
